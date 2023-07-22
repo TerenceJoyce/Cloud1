b38fc7ad88b8750a1ded601c558bb97e3c67fe54
Davide-Balzani-Test <davide.balzani@davide.balzani>
2023-07-22 15:09:44 +0200

    Add project files.

---
+++ Cloud1/Program.vb
@@ -0,0 +1,14 @@
+Imports System
+
+Module Program
+
+    Sub Main(args As String())
+        Console.WriteLine("Application start")
+        Do
+            Console.WriteLine("Hello World!")
+            Threading.Thread.Sleep(1000)
+        Loop Until False
+        Console.WriteLine("Application end")
+    End Sub
+
+End Module
