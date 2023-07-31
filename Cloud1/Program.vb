7ada2aefcbfa6dd4179e9e3cfa397183291ac9f2
Davide-Balzani-Test <davide.balzani@fluid.srl>
2023-07-31 18:33:40 +0200

    adsf

--- Cloud1/Program.vb
+++ Cloud1/Program.vb
@@ -5,7 +5,7 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("Application start")
         Do
-            Console.WriteLine("Hello World davide2!")
+            Console.WriteLine("Hello World davide3!")
             Threading.Thread.Sleep(1000)
         Loop Until False
         Console.WriteLine("Application end")
