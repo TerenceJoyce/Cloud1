c9457983933952bbaf8c8a32fda932b9b0a5bb4d
Davide-Balzani-Test <davide.balzani@fluid.srl>
2023-08-01 19:02:37 +0200

    adsf

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,5 @@
 !/bin/bash
 
-# Replace these values with your desired image URL and environment variable name
-# IMAGE_URL="your-image-url/image:tag"
-# ENV_VAR_NAME="SERVICE_IMAGE_URL"
-
-# Check if docker-compose is installed
-if ! command -v docker-compose &> /dev/null; then
-  echo "docker-compose is not installed. Please install it and try again."
-  exit 1
-fi
-
 # Create the docker-compose.yml file
 cat > compose.yaml << EOF
 version: '3'
